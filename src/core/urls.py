c91c22bb88345192a24510a7e30cc24642d5f78d
Yannick Kagou <yannickkagou@gmail.com>
2024-05-12 21:55:33 +0100

    Feat: add the navbar

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, graph_view, files_view, notif_view
 
 app_name = 'core'
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('graph/', graph_view, name='graph'),
+    path('files/', files_view, name='files'),
+    path('notifications/', notif_view, name='notifications'),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 def home_view(request):
     hello = "Hello World, I'm Herrol"
     return render(request, 'core/index.html', {'hello': hello})
+
+def graph_view(request):
+    hello = "Hello World Graph, I'm Herrol"
+    return render(request, 'core/graph.html', {'hello': hello})
+
+def files_view(request):
+    hello = "Hello World Files, I'm Herrol"
+    return render(request, 'core/files.html', {'hello': hello})
+
+def notif_view(request):
+    hello = "Hello World Notifications, I'm Herrol"
+    return render(request, 'core/notif.html', {'hello': hello})
